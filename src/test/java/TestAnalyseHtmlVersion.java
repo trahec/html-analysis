24a7ed8dc4cb84c9d1c8df40c87b76d6eba9ae67
Christine Trahe <christine.trahe@hpe.com>
2017-04-23 11:44:56 +0200

    moved html version tests to separate class

---
+++ src/test/java/TestAnalyseHtmlVersion.java
@@ -0,0 +1,31 @@
+import app.HTMLAnalysis;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAnalyseHtmlVersion {
+
+    HTMLAnalysis htmlAnalysis = new HTMLAnalysis();
+
+    @Test
+    public void testHtmlVersionValidVersion(){
+        Document document = Jsoup.parse(
+                "<!DOCTYPE html PUBLIC \"ISO/IEC 15445:2000//DTD HTML//EN\">" +
+                        "<head><title>TestTitle</title></head>" +
+                        "<body><h1>Sample Heading</h1><p>Paragraph</p></body></html>");
+        htmlAnalysis.analyseHtmlVersion(document);
+        assertEquals("ISOIEC15445", htmlAnalysis.htmlData.htmlVersion);
+    }
+
+    @Test
+    public void testHtmlVersionInvalidVersion() {
+        Document document = Jsoup.parse(
+                "<!DOCTYPE INVALID>" +
+                        "<head><title>TestTitle</title></head>" +
+                        "<body><h1>Sample Heading</h1><p>Paragraph</p></body></html>");
+        htmlAnalysis.analyseHtmlVersion(document);
+        assertEquals("Invalid HTML Version", htmlAnalysis.htmlData.htmlVersion);
+    }
+}
--- src/test/java/TestHtmlAnalysis.java
+++ src/test/java/TestHtmlAnalysis.java
@@ -24,25 +24,4 @@ public class TestHtmlAnalysis {
         htmlAnalysis.analysePageTitle(document);
         assertEquals("TestTitle", htmlAnalysis.htmlData.pageTitle);
     }
-
-    //Test analyseHTMLVersion
-    @Test
-    public void testHtmlVersionValidVersion(){
-        Document document = Jsoup.parse(
-                "<!DOCTYPE html PUBLIC \"ISO/IEC 15445:2000//DTD HTML//EN\">" +
-                        "<head><title>TestTitle</title></head>" +
-                        "<body><h1>Sample Heading</h1><p>Paragraph</p></body></html>");
-        htmlAnalysis.analyseHtmlVersion(document);
-        assertEquals("ISOIEC15445", htmlAnalysis.htmlData.htmlVersion);
-    }
-
-    @Test
-    public void testHtmlVersionInvalidVersion() {
-        Document document = Jsoup.parse(
-                "<!DOCTYPE INVALID>" +
-                        "<head><title>TestTitle</title></head>" +
-                        "<body><h1>Sample Heading</h1><p>Paragraph</p></body></html>");
-        htmlAnalysis.analyseHtmlVersion(document);
-        assertEquals("Invalid HTML Version", htmlAnalysis.htmlData.htmlVersion);
-    }
 }
