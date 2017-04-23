2c7e3809740a36c54cafd50fc311cb2728198e58
Christine Trahe <christine.trahe@hpe.com>
2017-04-23 11:35:05 +0200

    moved analyse links tests to separate test class so that each unit test would only have one assert

---
+++ src/test/java/TestAnalyseLinks.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by trahec on 2017-04-23.
+ */
+public class TestAnalyseLinks {
+}
--- src/test/java/TestHtmlAnalysis.java
+++ src/test/java/TestHtmlAnalysis.java
@@ -1,5 +1,4 @@
 import app.HTMLAnalysis;
-import app.HTMLData;
 import org.jsoup.Jsoup;
 import static org.junit.Assert.*;
 import org.jsoup.nodes.Document;
@@ -66,20 +65,5 @@ public class TestHtmlAnalysis {
         assertEquals(0, htmlAnalysis.htmlData.numberOfh4Tags);
         assertEquals(1, htmlAnalysis.htmlData.numberOfh5Tags);
         assertEquals(1, htmlAnalysis.htmlData.numberOfh6Tags);
-    }
-
-    //Test analyseLinks
-    @Test
-    public void testAnalyseLinks(){
-        Document document = Jsoup.parse(
-                "<!DOCTYPE html><head><title>TestTitle</title></head><body> " +
-                        "<a href=\"http://www.amazon.com\">link text</a>\n" +
-                        "<a href=\"http://www.wikipedia.com\">link text</a>\n" +
-                        "<a href=\"http://https://www.amazon.com/b/ref=nav_cs_gc_registry?ie=UTF8&node=14069511011\">text</a>\n" +
-                        "</body></html>");
-        htmlAnalysis.analyseLinks(document, "http://amazon.com");
-        assertEquals(3, htmlAnalysis.htmlData.numberOfLinks);
-        assertEquals(2, htmlAnalysis.htmlData.numberOfInternalLinks);
-        assertEquals(1, htmlAnalysis.htmlData.numberOfExternalLinks);
     }
 }
